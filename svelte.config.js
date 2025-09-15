996604d6471729050ca4ac631a210d519675e4b9
Coder2195 <coder2195mail@gmail.com>
2025-09-14 23:23:32 -0400

    vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,13 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      images: {
+        formats: ["image/avif", "image/webp"],
+        minimumCacheTTL: 300,
+        domains: ["coder2195.dev", "beta.coder2195.dev", "www.coder2195.dev"],
+      },
+    }),
     alias: {
       "@/*": "./src/*",
       "$assets/*": "./src/assets/*",
