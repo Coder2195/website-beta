80819ec29120c5abe8ed93c99d7790062577780a
Coder2195 <coder2195mail@gmail.com>
2025-12-14 16:27:13 -0500

    remove yarn state

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,15 +2,15 @@
   import ProjectCard from "@/components/projects/project-card.svelte";
   import type { PageProps } from "./$types";
 
-  let { data }: PageProps = $props();
-  let { projects } = data;
+  const { data }: PageProps = $props();
+  const { projects } = $derived(data);
 </script>
 
 <main>
   <h1>Projects</h1>
   <div class="my-4 mb-8">Some of the stuff I have been working on...</div>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-    {#each data.projects.sort((a, b) => {
+    {#each projects.sort((a, b) => {
       // featured on top, then sort by date
       if (a.featured && !b.featured) return -1;
       if (!a.featured && b.featured) return 1;
